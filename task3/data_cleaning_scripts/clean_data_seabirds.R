f5c2dd797e2376c561a46c7f44bcfcaee1c7c871
Neil Allan <neilneeds@yahoo.co.uk>
2020-12-13 15:18:46 +0000

    Task 3 - 1st draft cleaning script complete

--- task3/data_cleaning_scripts/clean_data_seabirds.R
+++ task3/data_cleaning_scripts/clean_data_seabirds.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(readxl)
 library(dplyr)
 library(janitor)
+library(assertr)
 
 
 # create a vector of expected sheet names in seabirds.xls
@@ -22,26 +23,81 @@ stopifnot(
 rm("excel_sheet_names_expected", "excel_sheet_names_actual")
 
 
-
-
 # read in the data
 ship_data_by_record_id <- read_excel("raw_data/seabirds.xls", sheet = "Ship data by record ID")
 bird_data_by_record_id <- read_excel("raw_data/seabirds.xls", sheet = "Bird data by record ID")
 ship_data_codes <- read_excel("raw_data/seabirds.xls", sheet = "Ship data codes")
 bird_data_codes <- read_excel("raw_data/seabirds.xls", sheet = "Bird data codes")
 
-# clean bird_data_by_record_id column names with janitor
+
+
+# bird_data_by_record_id - clean column names with janitor
 bird_data_by_record_id_clean <-
   clean_names(bird_data_by_record_id)
 
-# change column titles for common and scientific names
+# ship_data_by_record_id - clean column names with janitor
+ship_data_by_record_id_clean <-
+  clean_names(ship_data_by_record_id)
+
+
+
+
+# ship_data_by_record_id_clean - check that values for latitude and longitude are valid
+ship_data_by_record_id_clean  %>% 
+  verify(is.na(lat) | (lat >= -90 & lat <= 90)) %>% 
+  verify(is.na(long) | (long >= -180 & long <= 180))
+
+
+
+
+# bird_data_by_record_id - change column titles for common and scientific names
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   rename("species_common_name" = "species_common_name_taxon_age_sex_plumage_phase") %>% 
   rename("species_scientific_name" = "species_scientific_name_taxon_age_sex_plumage_phase")
 
 
-# remove strings relating to age, sex and plumage from species_common_name
+# bird_data_by_record_id - remove strings relating to age, sex and plumage from species_common_name
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
-  mutate(species_common_name = str_remove_all(species_common_name, " AD[MF]*| SUBAD[MF]*| IMM[MF]*| JUV[MF]*| PL[1-9][MF]*| DRK[MF]*| INT[MF]*| LGHT[MF]*| WHITE[MF]*"))
+  mutate(species_common_name = str_remove_all(species_common_name, 
+        " AD[MF]*| SUBAD[MF]*| IMM[MF]*| JUV[MF]*| PL[1-9][MF]*| DRK[MF]*| INT[MF]*| LGHT[MF]*| WHITE[MF]*"))
+# remove any leading or trailing spaces from species_common_name
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_common_name = trimws(species_common_name, which = "both"))
+
+
+#  bird_data_by_record_id - remove strings relating to age, sex and plumage from species_scientific_name
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_scientific_name = str_remove_all(species_scientific_name, 
+        " AD[MF]*| SUBAD[MF]*| IMM[MF]*| JUV[MF]*| PL[1-9][MF]*| DRK[MF]*| INT[MF]*| LGHT[MF]*| WHITE[MF]*"))
+# remove any leading or trailing spaces from species_scientific_name
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_scientific_name = trimws(species_scientific_name, which = "both"))
+
+#  bird_data_by_record_id - remove strings relating to age, sex and plumage from species_abbreviation
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_abbreviation = str_remove_all(species_abbreviation, 
+        " AD[MF]*| SUBAD[MF]*| IMM[MF]*| JUV[MF]*| PL[1-9][MF]*| DRK[MF]*| INT[MF]*| LGHT[MF]*| WHITE[MF]*"))
+# remove any leading or trailing spaces from species_abbreviation
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_abbreviation = trimws(species_abbreviation, which = "both"))
+
+
+
+
+# bird_data_by_record_id - write output to a csv
+write_csv(bird_data_by_record_id_clean,"clean_data/bird_data_by_record_id_clean.csv")
+
+# ship_data_by_record_id - write output to a csv
+write_csv(ship_data_by_record_id_clean,"clean_data/ship_data_by_record_id_clean.csv")
+
+
+
+
+
