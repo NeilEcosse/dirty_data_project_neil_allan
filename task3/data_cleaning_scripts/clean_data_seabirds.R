cf7deb8048d2fc65d01328e3424873ee7e969e32
Neil Allan <neilneeds@yahoo.co.uk>
2020-12-13 18:24:13 +0000

    task  cleaning file update - set count to 0 for NA

--- task3/data_cleaning_scripts/clean_data_seabirds.R
+++ task3/data_cleaning_scripts/clean_data_seabirds.R
@@ -3,6 +3,7 @@ library(readxl)
 library(dplyr)
 library(janitor)
 library(assertr)
+library(here)
 
 
 # create a vector of expected sheet names in seabirds.xls
@@ -58,14 +59,14 @@ ship_data_by_record_id_clean  %>%
 
 
 
-# bird_data_by_record_id - change column titles for common and scientific names
+# bird_data_by_record_id_clean - change column titles for common and scientific names
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   rename("species_common_name" = "species_common_name_taxon_age_sex_plumage_phase") %>% 
   rename("species_scientific_name" = "species_scientific_name_taxon_age_sex_plumage_phase")
 
 
-# bird_data_by_record_id - remove strings relating to age, sex and plumage from species_common_name
+# bird_data_by_record_id_clean - remove strings relating to age, sex and plumage from species_common_name
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   mutate(species_common_name = str_remove_all(species_common_name, 
@@ -74,9 +75,15 @@ bird_data_by_record_id_clean <-
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   mutate(species_common_name = trimws(species_common_name, which = "both"))
+# bird_data_by_record_id_clean - remove brackets from species_common_name for "NO BIRDS..." records
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_common_name = str_remove_all(species_common_name,"\\["))
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>% 
+  mutate(species_common_name = str_remove_all(species_common_name,"\\]"))
 
-
-#  bird_data_by_record_id - remove strings relating to age, sex and plumage from species_scientific_name
+#  bird_data_by_record_id_clean - remove strings relating to age, sex and plumage from species_scientific_name
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   mutate(species_scientific_name = str_remove_all(species_scientific_name, 
@@ -86,7 +93,7 @@ bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   mutate(species_scientific_name = trimws(species_scientific_name, which = "both"))
 
-#  bird_data_by_record_id - remove strings relating to age, sex and plumage from species_abbreviation
+#  bird_data_by_record_id_clean - remove strings relating to age, sex and plumage from species_abbreviation
 bird_data_by_record_id_clean <-
   bird_data_by_record_id_clean %>% 
   mutate(species_abbreviation = str_remove_all(species_abbreviation, 
@@ -99,10 +106,22 @@ bird_data_by_record_id_clean <-
 
 
 
-# bird_data_by_record_id_clean - write output to a csv
+# bird_data_by_record_id_clean_clean - change NA to 0 in numeric 'count' columns
+bird_data_by_record_id_clean <-
+  bird_data_by_record_id_clean %>%
+    mutate(count = if_else(is.na(count),0,count)) %>%
+    mutate(nfeed = if_else(is.na(nfeed),0,nfeed)) %>%
+    mutate(nsow = if_else(is.na(nsow),0,nsow)) %>%
+    mutate(nsoice = if_else(is.na(nsoice),0,nsoice)) %>%
+    mutate(nflyp = if_else(is.na(nflyp),0,nflyp)) %>%
+    mutate(nacc = if_else(is.na(nacc),0,nacc)) %>%
+    mutate(nfoll = if_else(is.na(nfoll),0,nfoll)) 
+
+
+# bird_data_by_record_id_clean_clean - write output to a csv
 write_csv(bird_data_by_record_id_clean,"clean_data/bird_data_by_record_id_clean.csv")
 
-# ship_data_by_record_id_clean - write output to a csv
+# ship_data_by_record_id_clean_clean - write output to a csv
 write_csv(ship_data_by_record_id_clean,"clean_data/ship_data_by_record_id_clean.csv")
 
 
