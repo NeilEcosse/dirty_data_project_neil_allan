5dd22bec928414c9197c405e9d24f26ec265ff30
Neil Allan <neilneeds@yahoo.co.uk>
2020-12-16 13:49:04 +0000

    task4 country cleaning code first draft

--- task4/02_data_cleaning_scripts/clean_data_2016.R
+++ task4/02_data_cleaning_scripts/clean_data_2016.R
@@ -6,8 +6,8 @@ library(assertr)
 
 
 
-# create a vector of actual sheet names in boing-boing-candy-2015.xlsx
-excel_sheet_names_actual <- c(excel_sheets("01_raw_data/boing-boing-candy-2015.xlsx"))
+# create a vector of actual sheet names in boing-boing-candy-2016.xlsx
+excel_sheet_names_actual <- c(excel_sheets("01_raw_data/boing-boing-candy-2016.xlsx"))
 
 
 # return an error if the expected sheet name can't be found
@@ -20,13 +20,13 @@ rm("excel_sheet_names_actual")
 
 
 # read in the data
-candy_2015_clean <-
-  read_excel("01_raw_data/boing-boing-candy-2015.xlsx",
+candy_2016_clean <-
+  read_excel("01_raw_data/boing-boing-candy-2016.xlsx",
              sheet = "Form Responses 1")
 
 # drop unwanted variables, create year and unique_id columns
-candy_2015_clean <-
-  candy_2015_clean %>% 
+candy_2016_clean <-
+  candy_2016_clean %>% 
   select("Timestamp":"[York Peppermint Patties]" ) %>% 
   mutate(year = substring(Timestamp,1,4)) %>% 
   mutate(unique_id = paste(Timestamp,
@@ -35,11 +35,10 @@ candy_2015_clean <-
                            sep = "_" ))
 
 
-
 # check if there are dates from more than one year - create error message if there are
 
 year_comparison <-
-  candy_2015_clean %>% 
+  candy_2016_clean %>% 
   summarise(max_year = max(year), min_year = min(year))
 
 stopifnot(year_comparison$max_year == year_comparison$min_year)
@@ -49,57 +48,78 @@ rm(year_comparison)
 
 
 
+
 # use pivot_longer to convert candy columns to rows - make it tidy and easier to
 #combine with data for other years
 
-candy_2015_clean <- 
-  candy_2015_clean %>% 
-  pivot_longer(cols = "[Butterfinger]":"[York Peppermint Patties]",
+candy_2016_clean <- 
+  candy_2016_clean %>% 
+  pivot_longer(cols = "[100 Grand Bar]":"[York Peppermint Patties]",
                names_to = "candy_name",
                values_to = "rating",)
 
 # clean column names
-candy_2015_clean <-
-  clean_names(candy_2015_clean)
+candy_2016_clean <-
+  clean_names(candy_2016_clean)
 
 # rename selected columns
-candy_2015_clean <-
-  candy_2015_clean %>% 
+candy_2016_clean <-
+  candy_2016_clean %>% 
   rename("date" = "timestamp") %>% 
+  rename("going_trick_or_treating" = "are_you_going_actually_going_trick_or_treating_yourself") %>%
+  rename("gender" = "your_gender") %>%
   rename("age_imported" = "how_old_are_you") %>% 
-  rename("going_trick_or_treating" = "are_you_going_actually_going_trick_or_treating_yourself")
+  rename("country_imported" = "which_country_do_you_live_in") %>%
+  rename("state_province_county_imported" = "which_state_province_county_do_you_live_in") 
 
 
 # delete observations where rating is NA - these are 'blank' rows created by pivot longer, not required
-candy_2015_clean <-
-  candy_2015_clean %>% 
+candy_2016_clean <-
+  candy_2016_clean %>% 
   filter(!is.na(rating))
-  
+
 # remove punctuation and spaces from unique_id
-candy_2015_clean <-
-  candy_2015_clean %>% 
+candy_2016_clean <-
+  candy_2016_clean %>% 
   mutate(unique_id = str_remove_all(unique_id, " |\\.|\\,|\\:|\\-"))
 
 # remove square brackets from candy_name
-candy_2015_clean <-
-  candy_2015_clean %>% 
+candy_2016_clean <-
+  candy_2016_clean %>% 
   mutate(candy_name = str_remove_all(candy_name,"\\[")) %>% 
   mutate(candy_name = str_remove_all(candy_name,"\\]"))
 
 
 # create a numeric age column
-candy_2015_clean <-
-  candy_2015_clean %>% 
+candy_2016_clean <-
+  candy_2016_clean %>% 
   mutate(age = as.numeric(str_extract(age_imported, "[0-9]+")))
 
 
 
 # convert ages greater than 120 to NA
-candy_2015_clean <-
-  candy_2015_clean %>% 
- mutate(age = if_else(age > 120, NA_real_, age))
+candy_2016_clean <-
+  candy_2016_clean %>% 
+  mutate(age = if_else(age > 120, NA_real_, age))
 
 
 
 # write output to csv file
-write_csv(candy_2015_clean, "03_clean_data/candy_2015_clean.csv")
+write_csv(candy_2016_clean, "03_clean_data/candy_2016_clean.csv")
+
+check_country_us <-
+  candy_2016_clean %>%
+  filter(str_detect(country_imported, "[uU][sS][aA]")) %>% 
+  group_by(country_imported) %>%
+  summarise(count  = n()) %>% 
+  arrange(country_imported)
+
+candy_2016_clean <-
+  candy_2016_clean %>%
+  select(date:age) %>% 
+  mutate(country = case_when(
+                    (str_detect(country_imported, "Not") & str_detect(country_imported, "[uU][sS][aA]")) ~ NA_character_,
+                    str_detect(country_imported, "[uU][sS][aA]") ~ "USA"
+                    ) 
+                    )
+
