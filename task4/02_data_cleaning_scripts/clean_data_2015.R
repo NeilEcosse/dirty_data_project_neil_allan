fa361beaf539cf5336bafde71ae0442fe69dc5b4
Neil Allan <neilneeds@yahoo.co.uk>
2020-12-16 16:53:27 +0000

    task4 - all years joined in analysis file

--- task4/02_data_cleaning_scripts/clean_data_2015.R
+++ task4/02_data_cleaning_scripts/clean_data_2015.R
@@ -24,12 +24,13 @@ candy_2015_clean <-
   read_excel("01_raw_data/boing-boing-candy-2015.xlsx",
              sheet = "Form Responses 1")
 
-# drop unwanted variables, create year, country and unique_id columns
+# drop unwanted variables, create year, country, gender and unique_id columns
 candy_2015_clean <-
   candy_2015_clean %>% 
   select("Timestamp":"[York Peppermint Patties]" ) %>% 
   mutate(year = substring(Timestamp,1,4)) %>% 
-  mutate(country = NA_character_) %>% 
+  mutate(country = NA_character_) %>%
+  mutate(gender = NA_character_) %>%
   mutate(unique_id = paste(Timestamp,
                            substring(`How old are you?`,1,2),
                            `Are you going actually going trick or treating yourself?`,
